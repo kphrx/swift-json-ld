d9f476f935612ba635f32a5015bb1cb246f1bc91
kPherox <kphrx@kpherox.dev>
2026-03-14 01:14:07 +0900

    fix(JSONLD): change to internal `Dictionary.mapValuesWithTypedThrows`

--- Sources/JSONLD/Extensions/Dictionary+mapValuesWithTypedThrows.swift
+++ Sources/JSONLD/Extensions/Dictionary+mapValuesWithTypedThrows.swift
@@ -4,7 +4,7 @@
 extension Dictionary {
   // NOTE: This extension is a temporary workaround and will be fixed in a future Swift release.
   //       Fixed in swiftlang/swift#86894 "Typed Throws Adoption for Several Dictionary APIs"
-  public func mapValuesWithTypedThrows<T, E: Error>(
+  func mapValuesWithTypedThrows<T, E: Error>(
     _ transform: (Value) throws(E) -> T
   ) throws(E) -> [Key: T] {
     try .init(
